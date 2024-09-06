321a2853c2f3799aca300815f2ec8a23cbb0c0b4
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-09-06 14:42:13 +0600

    environment variale added

--- src/Pages/Firebase/Firebase.config.js
+++ src/Pages/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBYA24clSs69xp8eIZTM_JwXQWjC_D7pEI",
-  authDomain: "bookheaven-914d3.firebaseapp.com",
-  projectId: "bookheaven-914d3",
-  storageBucket: "bookheaven-914d3.appspot.com",
-  messagingSenderId: "482698605340",
-  appId: "1:482698605340:web:edc3cca9bf038ec02c00d9",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
