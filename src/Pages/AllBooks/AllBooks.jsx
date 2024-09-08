8d35609992ba2d1af87b808d53551e08d65a3b0d
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-09-08 22:18:07 +0600

    started updating book data

--- src/Pages/AllBooks/AllBooks.jsx
+++ src/Pages/AllBooks/AllBooks.jsx
@@ -5,6 +5,16 @@ const AllBooks = () => {
   const data = useLoaderData();
   return (
     <div className="my-12">
+      <div className="text-center my-8">
+        <h2 className="lg:text-3xl text-xl font-bold">
+          Get all our books from here.
+        </h2>
+        <p className="text-gray-400 lg:max-w-2xl mx-auto py-2">
+          We have a huge collection of book in our library. Please select any
+          book and start reading . Dont forget to bring your library card .
+          Happy Learning.
+        </p>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:mx-8">
         {data?.map((card) => (
           <AllBooksCard key={card._id} card={card} />
--- src/Pages/Update/UpdateBook.jsx
+++ src/Pages/Update/UpdateBook.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 import Swal from "sweetalert2";
 import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const UpdateBook = () => {
   const {
@@ -18,7 +19,7 @@ const UpdateBook = () => {
   console.log(updateData);
 
   return (
-    <div className="bg-lightGreen">
+    <div className="bg-lightGreen min-h-screen">
       <section className="p-4 py-12 lg:px-24 mx-auto bg-lightGreen rounded-md ">
         <div>
           <h2 className="text-lg text-center font-semibold text-gray-700 capitalize ">
@@ -34,6 +35,7 @@ const UpdateBook = () => {
               </label>
               <input
                 id="bookName"
+                defaultValue={updateData?.bookName}
                 {...register("bookName")}
                 type="text"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-themeColor border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40   focus:outline-none focus:ring"
@@ -47,21 +49,9 @@ const UpdateBook = () => {
               <input
                 id="photo"
                 type="text"
+                defaultValue={updateData?.photo}
                 {...register("photo", { required: true })}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-themeColor border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
-              />
-            </div>
-
-            <div>
-              <label className="text-gray-700 " htmlFor="quantity">
-                Quantity
-              </label>
-              <input
-                id="quantity"
-                {...register("quantity")}
-                type="number"
-                min={0}
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-themeColor border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
               />
             </div>
 
@@ -71,35 +61,8 @@ const UpdateBook = () => {
               </label>
               <input
                 id="author"
+                defaultValue={updateData?.author}
                 {...register("author")}
-                type="text"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-themeColor border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40   focus:outline-none focus:ring"
-              />
-            </div>
-            <div className="">
-              <label className="text-gray-700  " htmlFor="category">
-                Category
-              </label>
-              <select
-                className="select bg-themeColor text-deepGreen w-full"
-                name="category"
-                {...register("category")}
-                id="category"
-              >
-                <option value="Novel">Novel</option>
-                <option value="Thriller">Thriller</option>
-                <option value="History">History</option>
-                <option value="Drama">Drama</option>
-                <option value="SciFi">Sci-Fi</option>
-              </select>
-            </div>
-            <div>
-              <label className="text-gray-700  " htmlFor="shortDescription">
-                Short Description
-              </label>
-              <input
-                id="shortDescription"
-                {...register("shortDescription")}
                 type="text"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-themeColor border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40   focus:outline-none focus:ring"
               />
@@ -110,23 +73,31 @@ const UpdateBook = () => {
               </label>
               <input
                 id="rating"
-                {...register("rating", { required: true })}
+                {...register("rating")}
                 type="number"
+                defaultValue={updateData?.rating}
                 min={0}
                 max={5}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-themeColor border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40   focus:outline-none focus:ring"
               />
             </div>
-            <div>
-              <label className="text-gray-700  " htmlFor="moreAbout">
-                More About This Book
+            <div className="col-span-1 lg:col-span-2">
+              <label className="text-gray-700  " htmlFor="category">
+                Category
               </label>
-              <input
-                id="moreAbout"
-                {...register("more")}
-                type="text"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-themeColor border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40   focus:outline-none focus:ring"
-              />
+              <select
+                className="select bg-themeColor text-deepGreen w-full"
+                name="category"
+                {...register("category")}
+                id="category"
+                defaultValue={updateData?.category}
+              >
+                <option value="Novel">Novel</option>
+                <option value="Thriller">Thriller</option>
+                <option value="History">History</option>
+                <option value="Drama">Drama</option>
+                <option value="SciFi">Sci-Fi</option>
+              </select>
             </div>
           </div>
 
